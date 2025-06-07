f879060537824c12cd37e946c753970a6a4d84fd
MasanoriIwakura <iwakura.masa@gmail.com>
2025-06-07 17:48:35 +0900

    CSVファイル処理の開始メッセージを追加

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { processCsv, CsvRow } from "./csv-stream";
 const filePath = path.resolve(__dirname, "sample.csv");
 
 async function main() {
+  console.log("CSVファイルの処理を開始します:", filePath);
+
   await processCsv(filePath, async (row: CsvRow) => {
     console.log("処理中の行:", row);
   });
